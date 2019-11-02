b919f48517581b65f82924819149407543c3a6dd
Moke <33320332+mokesher@users.noreply.github.com>
2019-11-02 15:43:45 +0800

    Create message.py
    爬取评论，筛选关键词

---
+++ B站/message.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import requests, json, re
+import time
+
+
+def run(url):
+    res = requests.get(url,headers=headers).text.split("(",1)[1].rsplit(")",1)[0]
+    res = json.loads(res)["data"]["replies"]
+    for i in res:
+        content = i["content"]["message"]
+        if "bgm" in content:
+            print(content)
+        elif "音乐" in content:
+            print(content)
+
+
+
+headers = {***}
+
+for page in range(0,100):
+    print(page)
+    url = f"https://api.bilibili.com/x/v2/reply?callback=jQuery17208677126098755512_1571146595675&jsonp=jsonp&pn={page}&type=1&oid=71327501&sort=2&={100*int(time.time())}"
+    run(url)
